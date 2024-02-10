a2871e071245b73a4cf37de713c0dd9612ad0a79
rfloarea <account@rfloarea.net>
2024-02-10 15:33:38 -0500

    script: locate new book from array and add properties to new elements

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ const dialog = document.querySelector('.dialog');
 const saveBtn = document.querySelector('.saveBtn');
 
 addBookBtn.addEventListener("click", () => dialog.showModal());
+dialog.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addNewBookToLibrary();
+    buildNewBookElement();
+});
 saveBtn.addEventListener("click", () => {
-    getNewBookInputs();
     dialog.close();
 });
+
 
 
 // book constructor to make the book objects
@@ -21,7 +26,7 @@ function Book(title, author, pages, read, notes) {
     this.notes = notes;
 };
 
-function getNewBookInputs() {
+function addNewBookToLibrary() {
     const titleInput = document.querySelector('#title').value;
     const authorInput = document.querySelector('#author').value;
     const pagesInput = document.querySelector('#pages').value;
@@ -31,58 +36,59 @@ function getNewBookInputs() {
     myLibrary.push(newBook);
     console.log(newBook);
     console.log(myLibrary);
+};
+    
+function buildNewBookElement() {
+    
+    const Book = myLibrary[myLibrary.length - 1];
+    console.log(Book);
+    
+    const shelves = document.querySelector('.shelves');
 
-    buildNewBook(newBook);
+    const newBookElement = document.createElement('div');
+    newBookElement.classList.add('book');
+    shelves.appendChild(newBookElement);
 
-    function buildNewBook() {
-        
-        const shelves = document.querySelector('.shelves');
-    
-        const newBookElement = document.createElement('div');
-        newBookElement.classList.add('book');
-        shelves.appendChild(newBookElement);
-    
-        const newBookInfoElement = document.createElement('ul');
-        newBookInfoElement.classList.add('bookInfo');
-        newBookElement.appendChild(newBookInfoElement);
-    
-        const newTitleElement = document.createElement('li');
-        newTitleElement.setAttribute('id', 'title');
-        newBookInfoElement.appendChild(newTitleElement);
-        newTitleElement.textContent = newBook.title;
-    
-        const newAuthorElement = document.createElement('li');
-        newAuthorElement.setAttribute('id', 'author');
-        newBookInfoElement.appendChild(newAuthorElement);
-        newAuthorElement.textContent = newBook.author;
-    
-        const newPagesElement = document.createElement('li');
-        newPagesElement.setAttribute('id', 'pages');
-        newBookInfoElement.appendChild(newPagesElement);
-        newPagesElement.textContent = newBook.pages;
-    
-        const newReadElement = document.createElement('li');
-        newReadElement.setAttribute('id', 'read');
-        newBookInfoElement.appendChild(newReadElement);
-        newReadElement.textContent = newBook.read;
-    
-        const newNotesElement = document.createElement('li');
-        newNotesElement.setAttribute('id', 'notes');
-        newBookInfoElement.appendChild(newNotesElement);
-        newNotesElement.textContent = newBook.notes;
-    
-        const newBookBtns = document.createElement('button');
-        newBookBtns.classList.add('bookBtns');
-        newBookElement.appendChild(newBookBtns);
-    
-        const newEditBtn = document.createElement('button');
-        newEditBtn.classList.add('editBtn');
-        newBookBtns.appendChild(newEditBtn);
-        newEditBtn.textContent = "edit";
-    
-        const newDeleteBtn = document.createElement('button');
-        newDeleteBtn.classList.add('deleteBtn');
-        newBookBtns.appendChild(newDeleteBtn);
-        newDeleteBtn.textContent = "delete";
-    }
+    const newBookInfoElement = document.createElement('ul');
+    newBookInfoElement.classList.add('bookInfo');
+    newBookElement.appendChild(newBookInfoElement);
+
+    const newTitleElement = document.createElement('li');
+    newTitleElement.setAttribute('id', 'title');
+    newBookInfoElement.appendChild(newTitleElement);
+    newTitleElement.textContent = Book.title;
+
+    const newAuthorElement = document.createElement('li');
+    newAuthorElement.setAttribute('id', 'author');
+    newBookInfoElement.appendChild(newAuthorElement);
+    newAuthorElement.textContent = Book.author;
+
+    const newPagesElement = document.createElement('li');
+    newPagesElement.setAttribute('id', 'pages');
+    newBookInfoElement.appendChild(newPagesElement);
+    newPagesElement.textContent = Book.pages;
+
+    const newReadElement = document.createElement('li');
+    newReadElement.setAttribute('id', 'read');
+    newBookInfoElement.appendChild(newReadElement);
+    newReadElement.textContent = Book.read;
+
+    const newNotesElement = document.createElement('li');
+    newNotesElement.setAttribute('id', 'notes');
+    newBookInfoElement.appendChild(newNotesElement);
+    newNotesElement.textContent = Book.notes;
+
+    const newBookBtns = document.createElement('button');
+    newBookBtns.classList.add('bookBtns');
+    newBookElement.appendChild(newBookBtns);
+
+    const newEditBtn = document.createElement('button');
+    newEditBtn.classList.add('editBtn');
+    newBookBtns.appendChild(newEditBtn);
+    newEditBtn.textContent = "edit";
+
+    const newDeleteBtn = document.createElement('button');
+    newDeleteBtn.classList.add('deleteBtn');
+    newBookBtns.appendChild(newDeleteBtn);
+    newDeleteBtn.textContent = "delete";
 };
