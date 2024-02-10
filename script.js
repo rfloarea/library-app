2f09e27df54114295a3a0ed966e166c3e9e4a932
rfloarea <account@rfloarea.net>
2024-02-10 07:17:07 -0500

    script: basic outline of user generated book objects

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const myLibrary = [];
 
 // book constructor to make the book objects
-
 function Book(title, author, pages, read, notes) {
     this.title = title;
     this.author = author;
@@ -10,3 +9,22 @@ function Book(title, author, pages, read, notes) {
     this.read = read;
     this.notes = notes;
 };
+
+// eventlistener on "add a book" opens a form
+// submitting that form pushes that data into these values below
+const newBook = Object.create(Book);
+// this is dummy data
+newBook.title = "Babel";
+newBook.author = "R.F. Kuang";
+newBook.pages = "500 pages";
+newBook.read = "I have read";
+newBook.notes = "This book is amazing!"
+
+// then we add that newBook to our array
+// then generate and display a new element at newBook's index
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary(newBook);
+console.log(myLibrary);
