21549707e360854a92751200d91c085fb00601c1
rfloarea <account@rfloarea.net>
2024-02-10 14:47:16 -0500

    fix: add id attr to newBook info elements

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const addBookBtn = document.querySelector('.addBookBtn');
 const dialog = document.querySelector('.dialog');
 const saveBtn = document.querySelector('.saveBtn');
 
-addBookBtn.addEventListener("click", () => buildNewBook(newBook));
-saveBtn.addEventListener("click", () => dialog.close());
+addBookBtn.addEventListener("click", () => dialog.showModal());
+saveBtn.addEventListener("click", () => {
+    getNewBookInputs();
+    dialog.close();
+});
+
 
 // book constructor to make the book objects
 function Book(title, author, pages, read, notes) {
@@ -17,77 +21,68 @@ function Book(title, author, pages, read, notes) {
     this.notes = notes;
 };
 
-
-// should i wrap this in a func? fine, but when do we invoke it?
-// on "add a book"
-const newBook = Object.create(Book);
-// this is dummy data
-newBook.title = "Babel"; // form data from "title"
-newBook.author = "R.F. Kuang"; // form data from "author"
-newBook.pages = "500 pages"; // form data from "pages"
-newBook.read = "I have read"; // form data from "read"
-newBook.notes = "This book is amazing!"; // form data from "notes"
-
-
-function getNewBookInputs(newBook) {
- 
-}
-
-
-
-
-;
-// then generate and display a new element at newBook's index
-function buildNewBook(newBook) {
-    dialog.showModal();
-
+function getNewBookInputs() {
+    const titleInput = document.querySelector('#title').value;
+    const authorInput = document.querySelector('#author').value;
+    const pagesInput = document.querySelector('#pages').value;
+    const readInput = document.querySelector('#read').value;
+    const notesInput = document.querySelector('#notes').value;
+    const newBook = new Book(titleInput, authorInput, pagesInput, readInput, notesInput);
     myLibrary.push(newBook);
     console.log(newBook);
     console.log(myLibrary);
-    // find list object in array
 
-    // build a new element for that object
-    const shelves = document.querySelector('.shelves');
+    buildNewBook(newBook);
 
-    const newBookElement = document.createElement('div');
-    newBookElement.classList.add('book');
-    shelves.appendChild(newBookElement);
-
-    const newBookInfoElement = document.createElement('ul');
-    newBookInfoElement.classList.add('bookInfo');
-    newBookElement.appendChild(newBookInfoElement);
-
-    const newTitleElement = document.createElement('li');
-    newBookInfoElement.appendChild(newTitleElement);
-    newTitleElement.textContent = newBook.title;
-
-    const newAuthorElement = document.createElement('li');
-    newBookInfoElement.appendChild(newAuthorElement);
-    newAuthorElement.textContent = newBook.author;
-
-    const newPagesElement = document.createElement('li');
-    newBookInfoElement.appendChild(newPagesElement);
-    newPagesElement.textContent = newBook.pages;
-
-    const newReadElement = document.createElement('li');
-    newBookInfoElement.appendChild(newReadElement);
-    newReadElement.textContent = newBook.read;
-
-    const newNotesElement = document.createElement('li');
-    newBookInfoElement.appendChild(newNotesElement);
-    newNotesElement.textContent = newBook.notes;
-
-    const newBookBtns = document.createElement('button');
-    newBookBtns.classList.add('bookBtns');
-    newBookElement.appendChild(newBookBtns);
-
-    const newEditBtn = document.createElement('button');
-    newEditBtn.classList.add('editBtn');
-    newBookBtns.appendChild(newEditBtn);
-    newEditBtn.textContent = "edit";
-
-    const newDeleteBtn = document.createElement('button');
-    newDeleteBtn.classList.add('deleteBtn');
-    newBookBtns.appendChild(newDeleteBtn);
-    newDeleteBtn.textContent = "delete";
-}
+    function buildNewBook() {
+        
+        const shelves = document.querySelector('.shelves');
+    
+        const newBookElement = document.createElement('div');
+        newBookElement.classList.add('book');
+        shelves.appendChild(newBookElement);
+    
+        const newBookInfoElement = document.createElement('ul');
+        newBookInfoElement.classList.add('bookInfo');
+        newBookElement.appendChild(newBookInfoElement);
+    
+        const newTitleElement = document.createElement('li');
+        newTitleElement.setAttribute('id', 'title');
+        newBookInfoElement.appendChild(newTitleElement);
+        newTitleElement.textContent = newBook.title;
+    
+        const newAuthorElement = document.createElement('li');
+        newAuthorElement.setAttribute('id', 'author');
+        newBookInfoElement.appendChild(newAuthorElement);
+        newAuthorElement.textContent = newBook.author;
+    
+        const newPagesElement = document.createElement('li');
+        newPagesElement.setAttribute('id', 'pages');
+        newBookInfoElement.appendChild(newPagesElement);
+        newPagesElement.textContent = newBook.pages;
+    
+        const newReadElement = document.createElement('li');
+        newReadElement.setAttribute('id', 'read');
+        newBookInfoElement.appendChild(newReadElement);
+        newReadElement.textContent = newBook.read;
+    
+        const newNotesElement = document.createElement('li');
+        newNotesElement.setAttribute('id', 'notes');
+        newBookInfoElement.appendChild(newNotesElement);
+        newNotesElement.textContent = newBook.notes;
+    
+        const newBookBtns = document.createElement('button');
+        newBookBtns.classList.add('bookBtns');
+        newBookElement.appendChild(newBookBtns);
+    
+        const newEditBtn = document.createElement('button');
+        newEditBtn.classList.add('editBtn');
+        newBookBtns.appendChild(newEditBtn);
+        newEditBtn.textContent = "edit";
+    
+        const newDeleteBtn = document.createElement('button');
+        newDeleteBtn.classList.add('deleteBtn');
+        newBookBtns.appendChild(newDeleteBtn);
+        newDeleteBtn.textContent = "delete";
+    }
+};
